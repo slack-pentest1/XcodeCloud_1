d251f7664f1bccc122a4ad9bccbc8e2a6e32fecf
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-03-19 14:58:24 +0530

    Initial Commit

---
+++ XcodeCloud_1/XcodeCloud_1App.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloud_1App.swift
+//  XcodeCloud_1
+//
+//  Created by Test Ios on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloud_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
